322d9c4a88269a72851e579059afc3d33465d737
Ritin George D <ritingeorge.d@gmail.com>
2024-09-02 18:09:58 +0530

    init: v1.0.0

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json())
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
--- routes/removeBG.js
+++ routes/removeBG.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 router.post("/", async (req, res, next) => {
     const { imageData } = req.body;
-    if (!imageData)
-        return res.status(400).json({ error: "Image Not Provided" });
+    if (!imageData) return res.status(400).json({ error: "Image Not Provided" });
     const result = await fetchNoBgImage(imageData);
-    res.status(200).json({
+    if(!result) {
+        return res.status(500).json({error:"An error occured while fetching image"})
+    }
+    return res.status(200).json({
         statusCode: 200,
         message: "Bg removal successful",
         data: { imageSrc: result },
